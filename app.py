24574da896bfd52bd849a2fdc46afe87c3735d0f
Jina Kim <fghjiurt@naver.com>
2021-11-11 11:27:08 +0900

    HTML 연습

--- app.py
+++ app.py
@@ -1,7 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def index():
-    return "Hello, Heroku"
+    return render_template("index.html")
+
+
+@app.route("/profile")
+def profile():
+    my_self = ["김진아", "92.01.30", "대전광역시"]
+    like_foods = ["돼지고기 김치찌개", "회전초밥", "쌀국수"]
+    return render_template("profile.html", like_foods=like_foods, my_self=my_self)
+
+
+@app.route("/posts")
+def post_list():
+    return render_template("post_list.html")
